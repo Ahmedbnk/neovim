2a3c7d971c2616042f0a1d6e6c97760ada9a21ce
Ahmed Benkrara <abenkrar@c2r9p2.1337.ma>
2025-04-22 16:17:03 +0100

    nvim

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- Auto-save on leaving Insert mode, switching buffers, or losing focus
+--
+vim.api.nvim_create_autocmd(
+  { "InsertLeave", "BufLeave", "FocusLost" },
+  {
+    pattern = "*",
+    command = "silent! write",
+  }
+)
