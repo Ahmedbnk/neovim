a4e34a5049f69b5846597f5fe33cdba17d1a4aa8
Ahmed Benkrara <abenkrar@c2r9p2.1337.ma>
2025-04-22 16:37:03 +0100

    nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.options")
 require("config.lazy")
 -- Auto-save on leaving Insert mode, switching buffers, or losing focus
 --
-vim.api.nvim_create_autocmd(
-  { "InsertLeave", "BufLeave", "FocusLost" },
-  {
-    pattern = "*",
-    command = "silent! write",
+vim.api.nvim_create_autocmd({ "InsertLeave", "BufLeave", "FocusLost" }, {
+  pattern = "*",
+  command = "silent! write",
   }
 )
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set transparency for main UI components
+vim.cmd([[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight NormalFloat guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight VertSplit guibg=NONE ctermbg=NONE
+  highlight StatusLine guibg=NONE ctermbg=NONE
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+]])
